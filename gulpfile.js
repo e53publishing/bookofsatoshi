e1b9b3665b0dba1ea58f3cf03574eae376126dfb
Samuel <myhonor16@users.noreply.github.com>
2014-08-22 20:10:15 -0400

    added "he currently lives..."

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+
+var webserver = require('gulp-webserver');
+
+gulp.task('server', function() {
+	gulp.src('./')
+		.pipe(webserver({
+			port: 9000,
+			directoryListing: true
+		}));
+});
+
+gulp.task('default', ['server'], function() {
+});
